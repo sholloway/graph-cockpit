bea5219e55bb78d891f57c49e8c48814e6d521ba
Samuel Holloway <sholloway@gmail.com>
2016-07-28 07:36:49 -0500

    Upgraded Electron and engine.io-client to latest.
    Added MIT license.

--- README.md
+++ README.md
@@ -19,5 +19,25 @@ brew install node
 | prune    | Removes “extraneous” packages i.e. packages installed but not listed in package.json file. |
 | pack     | Creates an installable tarball from a package. This tarball can then be published on npm registry or installed in some other environment e.g. production. |
 
+## Development
+### Updating Dependencies
+1. Detect updates via `npm outdated`
+2. Change the dependencies version number to the desired version.
+3. `npm update --save`
+4. `npm update --dev`
+5. Run the tests.
+
 ## Related Resources
 * [Electron](http://electron.atom.io)
+
+### Security & Privacy
+* [NIST Authentication Guidelines](https://pages.nist.gov/800-63-3/)
+* [Password Strength Test](https://github.com/nowsecure/owasp-password-strength-test)
+* [Owasp Password Guidelines](https://www.owasp.org/index.php/Authentication_Cheat_Sheet#Implement_Proper_Password_Strength_Controls)
+* [Owasp Password Reset](https://www.owasp.org/index.php/Forgot_Password_Cheat_Sheet)
+* [Owasp Password Storage](https://www.owasp.org/index.php/Password_Storage_Cheat_Sheet)
+* [Scala jBCrypt Wrapper](https://github.com/t3hnar/scala-bcrypt)
+* [Broken Session](https://www.owasp.org/index.php/Top_10_2013-A2-Broken_Authentication_and_Session_Management)
+* [Owasp Session Management Guide](https://www.owasp.org/index.php/Session_Management_Cheat_Sheet)
+* [Owasp TLS Guide](https://www.owasp.org/index.php/Transport_Layer_Protection_Cheat_Sheet)
+* [Owas XSS Guide](https://www.owasp.org/index.php/XSS_(Cross_Site_Scripting)_Prevention_Cheat_Sheet)
--- src/main.js
+++ src/main.js
@@ -1,15 +1,14 @@
 'use strict';
 
 const electron = require('electron');
-const app = electron.app;  // Module to control application life.
-const BrowserWindow = electron.BrowserWindow;  // Module to create native browser window.
+const {app, BrowserWindow} = electron;
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
 var mainWindow = null;
 
 // Quit when all windows are closed.
-app.on('window-all-closed', function() {
+app.on('window-all-closed', () => {
   // On OS X it is common for applications and their menu bar
   // to stay active until the user quits explicitly with Cmd + Q
   if (process.platform != 'darwin') {
@@ -19,7 +18,7 @@ app.on('window-all-closed', function() {
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
-app.on('ready', function() {
+app.on('ready', () => {
   // Create the browser window.
   mainWindow = new BrowserWindow({width: 800, height: 600});
 
@@ -37,3 +36,20 @@ app.on('ready', function() {
     mainWindow = null;
   });
 });
+
+/*
+The goal.
+* [X] Add License.
+* [ ] Electron, React, Webpack Hello World.
+* [ ] Create top level menu from React Component.
+* [ ] Window Naviagtion via React Routes.
+* [ ] Stub out all Menu Items.
+* [ ] Stub out all Windows & Flows.
+* [ ] Integrate with Engine Harness.
+* [ ] Build Authentication Flow.
+* [ ] Establish TLS between Cockpit & Engine
+
+Decisions to make.
+* [ ] Use a CSS Preprocessor
+* [ ] Leverage a UX framework.
+*/
